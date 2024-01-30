59a52cda4ee8639d9156f7bdfe66b68baccb289c
linorwang <17005041213@163.com>
2024-01-30 16:49:17 +0800

    perf: 增加初始化配置时监听配置变化

--- common/global/config.go
+++ common/global/config.go
@@ -8,6 +8,8 @@
 package global
 
 import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"os"
@@ -20,6 +22,11 @@ func InitConfig() {
 	viper.SetConfigName("application")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(workDir + "/config")
+	// 监听配置变化,无需重启应用读取配置
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Println(in.Name, in.Op)
+	})
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic("加载配置文件错误")
